a60417615a86f8d99e420b6aeefc364c52bf17fe
Y1n_yng Hong <NathanYang557@gmail.com>
2024-09-16 19:12:15 +0000

    created wordle, doesnt work

---
+++ wordle.py
@@ -0,0 +1,28 @@
+word = "fault"
+
+# color for printing
+default = '\033[0m'
+green = '\033[92m'
+yellow = '\033[33m'
+
+def generate_hint(guess):
+    color = default
+    for i in range(5):
+        if guess[i]==word[i]:
+            color = green
+        elif guess[i] in word:
+            color = yellow
+        else:
+            color = default
+        hint = hint + color + guess[i] + default
+    return hint
+
+def game_loop():
+    userGuess = ""
+    for i in range(6):
+        userGuess = input("give a guess: ")
+        if userGuess == word:
+            print("congratulations")
+            break
+game_loop()
+print(generate_hint(userGuess))
