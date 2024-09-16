e1604c2c6537f350386567acf00c3da38d6e218a
Y1n_yng Hong <NathanYang557@gmail.com>
2024-09-16 19:19:37 +0000

    added code idk if it works

--- wordle.py
+++ wordle.py
@@ -8,7 +8,7 @@ yellow = '\033[33m'
 def generate_hint(guess):
     color = default
     for i in range(5):
-        if guess[i]==word[i]:
+        if (guess[i]==word[i]):
             color = green
         elif guess[i] in word:
             color = yellow
@@ -21,8 +21,9 @@ def game_loop():
     userGuess = ""
     for i in range(6):
         userGuess = input("give a guess: ")
+        print(generate_hint(userGuess))
         if userGuess == word:
             print("congratulations")
             break
 game_loop()
-print(generate_hint(userGuess))
+
